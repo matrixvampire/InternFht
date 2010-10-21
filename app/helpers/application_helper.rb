abd4164d1af45e4a584b18e2e2f2e4d97e83fbe2
st111226 <st111226@9f60f1da-b9b2-4a02-b53a-be042a2a9883>
2010-10-21 10:31:50 +0000

    Add some method on helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,42 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
   def nav_link( text, controller, action=:index )
     link_to_unless_current text, :controller => controller, :action => action
   end
+  
   def logged_in?
     User.logged_in?( session )
   end  
+  
   def logout
     session[:user_id] = nil
     flash[:notice] = "Logged out"
     redirect_to :action => "index", :controller => "main"
   end
+  
+  def is_admin?
+    if session[:user_id] != nil
+      if get_type_of_user == TYPE_ADMINISTRATOR
+        return true
+      end
+    end 
+    return false
+  end
+  
+  def get_type_of_user
+    if session[:user_id] != nil
+      currentUser = User.find(session[:user_id])
+      return currentUser.usertype
+    end
+    return nil
+  end
+  
+  def get_name_of_user
+    if session[:user_id] != nil
+      currentUserDetail = People.find(session[:user_id])
+      return currentUserDetail.firstname+" "+currentUserDetail.middlename+" "+currentUserDetail.lastname
+    end
+  end
+  
 end
