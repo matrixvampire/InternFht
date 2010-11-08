b063dac496e34c06f691a12c74079256277a5490
st111226 <st111226@9f60f1da-b9b2-4a02-b53a-be042a2a9883>
2010-11-08 16:56:10 +0000

    view source

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -27,4 +27,59 @@ class MainController < ApplicationController
     @subtitle = "#{APPLICATION_NAME}"
   end
   
+  def viewsource
+    
+    @sourceController = "";
+    dir = "./app/controllers/"
+    contains = Dir.new(dir).entries
+    contains.length.times do |i|  
+      if contains[i].include?(".rb")
+        @sourceController = @sourceController + "\n\n ********* " + contains[i] + " *********\n\n"
+        file = File.new("./app/controllers/"+contains[i],"r");
+        
+        while(line = file.gets)
+          @sourceController = @sourceController.concat(line);
+        end
+        file.close;
+      end
+    end
+  
+    @sourceModel = "";
+    dir = "./app/models/"
+    contains = Dir.new(dir).entries
+    contains.length.times do |i|  
+      if contains[i].include?(".rb")
+        @sourceModel = @sourceModel + "\n\n ********* " + contains[i] + " *********\n\n"
+        file = File.new("./app/models/"+contains[i],"r");
+        
+        while(line = file.gets)
+          @sourceModel = @sourceModel.concat(line);
+        end
+        file.close;
+      end
+    end
+    
+#    file = File.new("./app/controllers/user_controller.rb","r");
+#    @sourceController = "";
+#    while(line = file.gets)
+#      @sourceController = @sourceController.concat(line);
+#    end
+#    file.close;
+    
+    @sourceHtmlErb = "";
+    dir = "./app/views/user/"
+    contains = Dir.new(dir).entries
+    contains.length.times do |i|  
+      if contains[i].include?(".html.erb") or contains[i].include?(".rxml")
+        @sourceHtmlErb = @sourceHtmlErb + "\n\n ********* " + contains[i] + " *********\n\n"
+        file = File.new("./app/views/user/"+contains[i],"r");
+        
+        while(line = file.gets)
+          @sourceHtmlErb = @sourceHtmlErb.concat(line);
+        end
+        file.close;
+      end
+    end
+  end
+  
 end
